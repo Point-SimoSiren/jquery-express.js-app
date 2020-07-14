4833f9552e8aaa09586d0f5d2423ae291f2fdd80
Sirén Simo <simo.siren1@careeria.fi>
2020-07-14 03:39:25 +0300

    efektejä jQueryllä

--- public/index.js
+++ public/index.js
@@ -4,13 +4,28 @@ $(document).ready(function(){
     $("#b2").hide()
 
     $("#b1").click(function(){
-      $(".info").show()
-      $("#b2").show()
-      $("#b1").hide()
+      $(".info").fadeIn(2000);
+      $("#b1").hide();
+      $("#b2").fadeIn(2000);
+      
     })
     $("#b2").click(function(){
-      $(".info").hide()
-      $("#b1").show()
+      $(".info").fadeOut(1000)
       $(this).hide()
+      $("#b1").fadeIn(1000);
+     
+    })
+    $("#b3").click(function(){
+      $(".content").empty();
+      var txt1 = $("<p></p>").text("Teemme hiekkapuhalluksia, maalauksia ja kaikenlaisia autojen korjaustöitä");
+      var tx = $("<p></p>").text("Myymme jenkkiautojen purkuosia ja entisöimme vanhoja amerikanrautoja.");
+      $(".content").append(txt1, tx)
+      
+    })
+    $("#b4").click(function(){
+      $(".content").empty();
+      var txt2 = $("<p></p>").text("Tommi Ryynänen p. 0400-1394459");
+      var txt3 = $("<p></p>").text("mail@rustbros.fi");
+$(".content").append(txt2, txt3)
     })
     })
--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const express = require('express')
 
 const app = express()
 
-// Tehdään polkumääritys public kansioon 
 const polku = path.join(__dirname, './public')
 
-// Sanotaan että em. polussa on tiedostosisältö jota palvelin käyttää kun se saa http requestin
 app.use(express.static(polku))
 
 app.listen(3000, () => {
